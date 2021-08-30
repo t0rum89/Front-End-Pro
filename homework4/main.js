afeefd6aac2983396633dab804535496f3f7a06a
t0rum89 <kaliberda.r@gmail.com>
2021-08-30 13:50:25 +0300

    homework_4

--- homework4/main.js
+++ homework4/main.js
@@ -38,7 +38,7 @@ console.log(min(0, -1, 100, 500, 100500));
 // - Добавьте каждому объекту поле fullName, которое является конкатенацией имени и фамилии
 // - Сформируйте новый массив, который содержит только полное имя пользователей
 
-let users = [
+const users = [
   (person1 = {
     firstname: "Иван",
     lastname: "Иванов",
@@ -91,23 +91,76 @@ let users = [
   }),
 ];
 
-let result1 = users.filter(function (el) {
+const result1 = users.filter(function (el) {
   return el.age < 18;
 });
 console.log(result1);
 
-let result2 = users.forEach(function (el) {
-  return (users["fullname"] = `${users.firstname} + ${users.lastname}`);
+const result2 = users.slice();
+result2.forEach((el) => {
+  el.fullName = `${el.firstname} ${el.lastname}`;
 });
 console.log(result2);
+
+//второй вариант - используя метод map
+const result21 = users.map((el) => ({
+  ...el,
+  fullName: `${el.firstname} ${el.lastname}`,
+}));
+console.log(result21);
+
+let result3 = users.map((el) => ({
+  fullName: `${el.firstname} ${el.lastname}`,
+}));
+console.log(result3);
+
 // 4. Напишите функцию аналог метода массива shift. Функция удаляет из переданного в параметре массива первый элемент.
 
+function funcShift(arr) {
+  const newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr[i - 1] = arr[i];
+  }
+  return newArr;
+}
+
+// второй вариант
+
+function funcShift2(arr) {
+  const [, ...newArr] = arr;
+  return newArr;
+}
+
 // 5. Напишите функцию аналог метода массива push. Функция добавляет в конец переданного в параметре массив произвольное количество элементов.
+
+function funcPush() {
+  let newArr = [...arguments[0]];
+  for (let i = 1; i < arguments.length; i++) {
+    newArr = [...newArr, arguments[i]];
+  }
+  return newArr;
+}
+console.log(funcPush([1, 2, 3], 4, 5, 6));
 
 // 6. Напишите функцию аналог метода Object.assign(). Первый параметр функции - целевой объект, поля которого будут изменены или расширены. Остальные параметры - объекты-источники, полями которых будет расширяться целевой объект.
 // var source = {firstname: 'Tom', age: 10}
 // var s = extend(source, {firstname: 'John'}, {lastname: 'Doe'});
 // console.log(s); // {firstname: 'John', age: 10, lastname: 'Doe'}
+
+function funcAssign() {
+  let newArr2 = { ...arguments[0] };
+  for (let i = 1; i < arguments.length; i++) {
+    newArr2 = { ...newArr2, ...arguments[i] };
+  }
+  return newArr2;
+}
+console.log(
+  funcAssign(
+    { firstname: "Tom", age: 10 },
+    { firstname: "John" },
+    { lastname: "Doe" }
+  )
+);
 
 // 7. Напишите функцию setComment с параметрами: date, message, author. Дата и текст сообщения - обязательные параметры, если какой-то из них или оба отсутствуют, то выполнение функции должно обрываться, а пользователю выдаваться предупреждение (alert) о том, что данные переданы некорректно. Параметр author - опциональный, но должна происходить проверка: если параметр не передан, то вместо него подставляется значение ‘Anonymous’. Функция распечатывает в консоле текст в формате:
 // 				<имя_автора>, <дата>
@@ -122,3 +175,16 @@ console.log(result2);
 
 // Anonymous, 2016-12-22
 // You could do it better!
+
+function setComment(date = null, message = null, author = "Anonymous") {
+  if (!date || !date.length) {
+    alert("wrong");
+    return;
+  }
+  if (!message || !message.length) {
+    alert("wrong");
+    return;
+  }
+  return `${author}, ${date}\n${message}`;
+}
+console.log(setComment("2016-11-02", "Everything is ok"));
